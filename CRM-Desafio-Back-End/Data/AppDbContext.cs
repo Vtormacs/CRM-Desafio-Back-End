54403dc4fa7330c7ad16b942dc10a35dfbcb1747
Vtormacs <macskids98play@hotmail.com>
2024-12-18 18:58:22 -0300

    config da criacao das tabelas no banco e config da conexao com o banco

--- CRM-Desafio-Back-End/Data/AppDbContext.cs
+++ CRM-Desafio-Back-End/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CRM_Desafio_Back_End.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRM_Desafio_Back_End.Data
 {
@@ -7,5 +8,9 @@ namespace CRM_Desafio_Back_End.Data
         protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Movement> movements { get; set; }
     }
 }
