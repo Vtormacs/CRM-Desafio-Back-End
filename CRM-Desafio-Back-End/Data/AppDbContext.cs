67850d3a30ab3b4922222d70dd3ef35362bdbf27
Vtormacs <macskids98play@hotmail.com>
2024-12-18 17:37:23 -0300

    Created AppDbContext

---
+++ CRM-Desafio-Back-End/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRM_Desafio_Back_End.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
