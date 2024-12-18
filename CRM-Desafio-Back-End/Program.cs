0bd6759a090f0cff446148ca1f4169107edbd557
Vtormacs <macskids98play@hotmail.com>
2024-12-18 19:42:46 -0300

    Criacao do Service do User

--- CRM-Desafio-Back-End/Program.cs
+++ CRM-Desafio-Back-End/Program.cs
@@ -1,4 +1,5 @@
 using CRM_Desafio_Back_End.Data;
+using CRM_Desafio_Back_End.Services.User;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserInterface, UserService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
           options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString"), npgsqlOptions =>
