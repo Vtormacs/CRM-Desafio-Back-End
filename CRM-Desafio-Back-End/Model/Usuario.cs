f0ad529fc6036b849880db333f8da88c2f534b66
Vtormacs <macskids98play@hotmail.com>
2024-12-18 16:43:46 -0300

    Model Usuario

--- CRM-Desafio-Back-End/Model/Usuario.cs
+++ CRM-Desafio-Back-End/Model/Usuario.cs
@@ -1,6 +1,31 @@
-﻿namespace CRM_Desafio_Back_End.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_Desafio_Back_End.Model
 {
+    [Table("usuario")]
     public class Usuario
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public string email { get; private set; }
+        public DateOnly birthday { get; private set; }
+        public DateTime created_at { get; private set; }
+        public DateTime updated_at { get; private set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string name, string email, DateOnly birthday, DateTime created_at, DateTime updated_at)
+        {
+            this.name = name;
+            this.email = email;
+            this.birthday = birthday;
+            this.created_at = created_at;
+            this.updated_at = updated_at;
+        }
     }
 }
