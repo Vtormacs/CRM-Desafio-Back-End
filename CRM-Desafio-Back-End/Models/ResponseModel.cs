f89e4c4a9a2833623f2d1b7cbec68f928e3ed487
Vtormacs <macskids98play@hotmail.com>
2024-12-20 14:24:54 -0300

    feat(Models): Adicionar ResponseModel

---
+++ CRM-Desafio-Back-End/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM_Desafio_Back_End.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? dados { get; set; }
+        public string mensagem { get; set; } = string.Empty;
+        public bool status { get; set; } = true;
+    }
+}
