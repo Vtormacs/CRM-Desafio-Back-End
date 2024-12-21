71cfd5a2531c3c67a52b30ba07effa6d6f0d4969
Vtormacs <macskids98play@hotmail.com>
2024-12-21 12:59:41 -0300

    feat(Dtos): Criação de UserCriacaoDto - UserCriacaoDto tera dados como name(string), email(string) e birthday(DateOnly)

---
+++ CRM-Desafio-Back-End/Dtos/User/UserCriacaoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM_Desafio_Back_End.Dtos.User
+{
+    public class UserCriacaoDto
+    {
+        public string name { get; private set; }
+        public string email { get; private set; }
+        public DateOnly birthday { get; private set; }
+    }
+}
