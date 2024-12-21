240e79624daa7ba958897b6ce4368da84cb68267
Vtormacs <macskids98play@hotmail.com>
2024-12-21 15:34:50 -0300

    refactor(Dtos): Validação de email no Dto de UserCriacaoDto

--- CRM-Desafio-Back-End/Dtos/User/UserCriacaoDto.cs
+++ CRM-Desafio-Back-End/Dtos/User/UserCriacaoDto.cs
@@ -1,9 +1,12 @@
-﻿namespace CRM_Desafio_Back_End.Dtos.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM_Desafio_Back_End.Dtos.User
 {
     public class UserCriacaoDto
     {
-        public string name { get; private set; }
-        public string email { get; private set; }
-        public DateOnly birthday { get; private set; }
+        public string name { get; set; }
+        [EmailAddress(ErrorMessage = "The email field is not a valid e-mail address.")]
+        public string email { get; set; }
+        public DateOnly birthday { get; set; }
     }
 }
