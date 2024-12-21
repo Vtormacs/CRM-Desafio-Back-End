c6c2c6779f3192a8fcd0d079d4238cdca480c82e
Vtormacs <macskids98play@hotmail.com>
2024-12-20 22:33:45 -0300

    feat(Controllers): Endpoint de listagem de usuarios

--- CRM-Desafio-Back-End/Controllers/UserController.cs
+++ CRM-Desafio-Back-End/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CRM_Desafio_Back_End.Model;
+using CRM_Desafio_Back_End.Models;
+using CRM_Desafio_Back_End.Services.User;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_Desafio_Back_End.Controllers
@@ -7,5 +10,17 @@ namespace CRM_Desafio_Back_End.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserInterface _userInterface;
+        public UserController(IUserInterface userInterface)
+        {
+            _userInterface = userInterface;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseModel<List<UserModel>>>> ListarUsers()
+        {
+            var users = await _userInterface.listarUsers();
+            return Ok(users);
+        }
     }
 }
