faac1a7b8fd60144596a91d07c7a76c89ac91c99
Vtormacs <macskids98play@hotmail.com>
2024-12-23 12:49:32 -0300

    feat(Dtos): Criação do DTO de criação de produto - Com valores de name(string), amount(int) e value(decimal)

---
+++ CRM-Desafio-Back-End/Dtos/Product/ProductCriacaoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM_Desafio_Back_End.Dtos.Product
+{
+    public class ProductCriacaoDto
+    {
+        public string name { get; set; }
+        public int amount { get; set; }
+        public decimal value { get; set; }
+    }
+}
