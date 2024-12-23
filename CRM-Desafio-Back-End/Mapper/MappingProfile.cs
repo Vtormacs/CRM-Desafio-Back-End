1601ff7ea96000a9e334287957144b14294d7b60
Vtormacs <macskids98play@hotmail.com>
2024-12-23 15:03:54 -0300

    feat(Mapper): Criação de MappingProfile

---
+++ CRM-Desafio-Back-End/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CRM_Desafio_Back_End.Dtos.Product;
+using CRM_Desafio_Back_End.Model;
+
+namespace CRM_Desafio_Back_End.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ProductCriacaoDto, ProductModel>();
+            CreateMap<ProductModel, ProductViewDto>();
+        }
+    }
+}
