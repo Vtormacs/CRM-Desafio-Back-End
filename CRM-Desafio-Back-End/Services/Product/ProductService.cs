9598f95c236daa6e9ff15a22c618871230f510c3
Vtormacs <macskids98play@hotmail.com>
2024-12-23 17:40:12 -0300

    feat(ProductService): Estrutura de ProductService

--- CRM-Desafio-Back-End/Services/Product/ProductService.cs
+++ CRM-Desafio-Back-End/Services/Product/ProductService.cs
@@ -1,6 +1,26 @@
-﻿namespace CRM_Desafio_Back_End.Services.Product
+﻿using AutoMapper;
+using CRM_Desafio_Back_End.Dtos.Product;
+using CRM_Desafio_Back_End.Repositories.Product;
+
+namespace CRM_Desafio_Back_End.Services.Product
 {
     public class ProductService : IProductInterface
     {
+        public readonly IProductRepository _productRepository;
+        public readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+        public Task<ProductViewDto> criarProduto(ProductCriacaoDto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductViewDto>> listarProdutos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
