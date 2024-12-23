4ef1ed992ee7886024383b24a20378fc60904ed5
Vtormacs <macskids98play@hotmail.com>
2024-12-23 15:02:10 -0300

    feat(Dtos): Criação de Product View e User View

---
+++ CRM-Desafio-Back-End/Dtos/User/UserViewDto.cs
@@ -0,0 +1,14 @@
+﻿using CRM_Desafio_Back_End.Model;
+
+namespace CRM_Desafio_Back_End.Dtos.User
+{
+    public class UserViewDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public DateOnly birthday { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
