b18daa89391f6e7146af90f1480379527042309e
Vtormacs <macskids98play@hotmail.com>
2024-12-23 12:20:46 -0300

    docs(UserController): Documentação de todos endpoints de UserController

--- CRM-Desafio-Back-End/Controllers/UserController.cs
+++ CRM-Desafio-Back-End/Controllers/UserController.cs
@@ -7,16 +7,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_Desafio_Back_End.Controllers
 {
+    /// <summary>
+    /// Controlador para gerenciar operações relacionadas a usuários.
+    /// </summary>
     [Route("api/v1/users")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly IUserInterface _userInterface;
+
+        /// <summary>
+        /// Construtor para injetar dependências.
+        /// </summary>
+        /// <param name="userInterface">Interface de serviço de usuário.</param>
         public UserController(IUserInterface userInterface)
         {
             _userInterface = userInterface;
         }
 
+        /// <summary>
+        /// Lista todos os usuários.
+        /// </summary>
+        /// <returns>Modelo de resposta contendo a lista de usuários.</returns>
         [HttpGet]
         public async Task<ActionResult<ResponseModel<List<UserModel>>>> ListarUsers()
         {
@@ -24,6 +36,11 @@ namespace CRM_Desafio_Back_End.Controllers
             return Ok(users);
         }
 
+        /// <summary>
+        /// Busca um usuário pelo ID.
+        /// </summary>
+        /// <param name="id">ID do usuário.</param>
+        /// <returns>Modelo de resposta contendo o usuário.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<ResponseModel<UserModel>>> BuscarUserPorId(int id)
         {
@@ -31,6 +48,11 @@ namespace CRM_Desafio_Back_End.Controllers
             return Ok(user);
         }
 
+        /// <summary>
+        /// Cria um novo usuário.
+        /// </summary>
+        /// <param name="userCriacaoDto">Objeto DTO contendo as informações do usuário a ser criado.</param>
+        /// <returns>Modelo de resposta contendo a lista de usuários atualizada.</returns>
         [HttpPost]
         public async Task<ActionResult<ResponseModel<List<UserModel>>>> CriarUser([FromBody] UserCriacaoDto userCriacaoDto)
         {
@@ -43,8 +65,13 @@ namespace CRM_Desafio_Back_End.Controllers
             return Ok(users);
         }
 
+        /// <summary>
+        /// Exclui um usuário pelo ID.
+        /// </summary>
+        /// <param name="id">ID do usuário.</param>
+        /// <returns>Modelo de resposta contendo o usuário excluído.</returns>
         [HttpDelete("{id}")]
-        public async Task<ActionResult<ResponseModel<UserModel>>> ExcexcluirUser(int id)
+        public async Task<ActionResult<ResponseModel<UserModel>>> ExcluirUser(int id)
         {
             var user = await _userInterface.excluirUser(id);
             return Ok(user);
