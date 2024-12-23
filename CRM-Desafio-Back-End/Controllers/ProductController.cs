b2611919764593280fb1741e61ba5667f75db05e
Vtormacs <macskids98play@hotmail.com>
2024-12-23 17:56:01 -0300

    feat(ProductController): Criação de ProductController sem nenhum endpoint

---
+++ CRM-Desafio-Back-End/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_Desafio_Back_End.Controllers
+{
+    [Route("api/v1/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
