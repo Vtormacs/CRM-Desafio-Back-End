a894b73b24e3a836113132b1fd36c66114662684
Vtormacs <macskids98play@hotmail.com>
2024-12-27 13:06:35 -0300

    feat(Repository): Implementação do metodo para salvar movimento no banco de dados

--- CRM-Desafio-Back-End/Repositories/Movement/IMovementRepository.cs
+++ CRM-Desafio-Back-End/Repositories/Movement/IMovementRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace CRM_Desafio_Back_End.Repositories.Movement
+﻿using CRM_Desafio_Back_End.Model;
+
+namespace CRM_Desafio_Back_End.Repositories.Movement
 {
     public interface IMovementRepository
     {
+        Task<MovementModel> cadastrarMovimento(MovementModel movimento);
     }
 }
--- CRM-Desafio-Back-End/Repositories/Movement/MovementRpository.cs
+++ CRM-Desafio-Back-End/Repositories/Movement/MovementRpository.cs
@@ -1,6 +1,20 @@
-﻿namespace CRM_Desafio_Back_End.Repositories.Movement
+﻿using CRM_Desafio_Back_End.Data;
+using CRM_Desafio_Back_End.Model;
+
+namespace CRM_Desafio_Back_End.Repositories.Movement
 {
     public class MovementRpository : IMovementRepository
     {
+        private readonly AppDbContext _appDbContext;
+        public MovementRpository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<MovementModel> cadastrarMovimento(MovementModel movimento)
+        {
+            await _appDbContext.movements.AddAsync(movimento);
+            await _appDbContext.SaveChangesAsync();
+            return movimento;
+        }
     }
 }
