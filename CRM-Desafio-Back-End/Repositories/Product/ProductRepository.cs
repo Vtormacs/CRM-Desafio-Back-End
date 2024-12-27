61cd3164305b582c10b5013b164547698ec7d164
Vtormacs <macskids98play@hotmail.com>
2024-12-27 12:59:54 -0300

    docs(ProductRepository): Documentação de todas a função que se comunicão com o banco de dados

--- CRM-Desafio-Back-End/Repositories/Product/ProductRepository.cs
+++ CRM-Desafio-Back-End/Repositories/Product/ProductRepository.cs
@@ -4,14 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CRM_Desafio_Back_End.Repositories.Product
 {
+    /// <summary>
+    /// Repositório para gerenciar operações relacionadas a produtos no banco de dados.
+    /// </summary>
     public class ProductRepository : IProductRepository
     {
         private readonly AppDbContext _appDbContext;
+
+        /// <summary>
+        /// Construtor para injetar dependências.
+        /// </summary>
+        /// <param name="appDbContext">Contexto do banco de dados.</param>
         public ProductRepository(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
         }
 
+        /// <summary>
+        /// Cadastra um novo produto no banco de dados.
+        /// </summary>
+        /// <param name="produto">Modelo do produto a ser cadastrado.</param>
+        /// <returns>Modelo do produto cadastrado.</returns>
         public async Task<ProductModel> cadatrarProduto(ProductModel produto)
         {
             await _appDbContext.products.AddAsync(produto);
@@ -19,6 +32,10 @@ namespace CRM_Desafio_Back_End.Repositories.Product
             return produto;
         }
 
+        /// <summary>
+        /// Lista todos os produtos do banco de dados.
+        /// </summary>
+        /// <returns>Lista de produtos.</returns>
         public async Task<List<ProductModel>> listarProdutos()
         {
             return await _appDbContext.products.ToListAsync();
