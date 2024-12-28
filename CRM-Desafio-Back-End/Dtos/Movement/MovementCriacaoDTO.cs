0e89d779dfd733c4c6a6f408a2e0fe1b27ead6c9
Vtormacs <macskids98play@hotmail.com>
2024-12-28 14:46:04 -0300

    feat(DTOs): Criação do DTO de criação de movimentos

---
+++ CRM-Desafio-Back-End/Dtos/Movement/MovementCriacaoDTO.cs
@@ -0,0 +1,17 @@
+﻿using CRM_Desafio_Back_End.Models.Enums;
+
+namespace CRM_Desafio_Back_End.DTOs.Movement
+{
+    public class MovementCriacaoDTO
+    {
+        public int idUser { get; set; }
+        public PaymentType paymentType { get; set; }
+        public List<ProductMovementCriacaoDTO> products { get; set; }
+    }
+
+    public class  ProductMovementCriacaoDTO
+    {
+        public int idProduct { get; set; }
+        public int quantity { get; set; }
+    }
+}
